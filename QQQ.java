88991ba210a31774a7479cc20605210ffb43d977
syteser <savchenko.sergey.v@gmail.com>
2023-04-25 21:54:20 +0300

    24

--- QQQ.java
+++ QQQ.java
@@ -1,2 +1,5 @@
 public class QQQ {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
 }
--- webClass.java
+++ webClass.java
@@ -1,4 +1,5 @@
 public class webClass{
     public static void main(String[]args){
+        System.out.println(2);
     }
 }

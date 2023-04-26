59c5334799b3fb6d82c635caf216f3ef20133356
syteser <savchenko.sergey.v@gmail.com>
2023-04-26 21:23:39 +0300

    6

--- myProject/Animal.java
+++ myProject/Animal.java
@@ -5,6 +5,30 @@ public class Animal {
     private String name;
     private int weight;
 
+    public Animal(int age, String name, int weight) {
+        this.age = age;
+        this.name = name;
+        this.weight = weight;
+    }
 
+    public Animal(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
 
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
 }

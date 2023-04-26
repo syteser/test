1354429f9100b09283c7046e23c46b3be2804fad
syteser <savchenko.sergey.v@gmail.com>
2023-04-26 21:25:02 +0300

    6

--- myProject/QQQ.java
+++
@@ -1,7 +0,0 @@
-package myProject;
-
-public class QQQ {
-    public static void main(String[] args) {
-        System.out.println(1);
-    }
-}
--- myProject/webClass.java
+++
@@ -1,8 +0,0 @@
-package myProject;
-
-public class webClass{
-    public static void main(String[]args){
-        System.out.println(2);
-        System.out.println("test!!!!");
-    }
-}

f66b145715386ef88b50b9cddc780a910ed6e916
syteser <savchenko.sergey.v@gmail.com>
2023-04-30 20:31:35 +0300

    Добавил static методы getMax и getMin  класса MapMath

--- myProject/Main.java
+++ myProject/Main.java
@@ -1,7 +1,22 @@
 package myProject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("start....");
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1,2);
+        map.put(2,22);
+        map.put(3,0);
+        map.put(4,-12);
+        map.put(5,22);
+        map.put(6,12);
+
+        System.out.println(MapMath.getMin(map));
+        System.out.println(MapMath.getMax(map));
+
     }
 }
---
+++ myProject/MapMath.java
@@ -0,0 +1,38 @@
+package myProject;
+
+import java.util.*;
+
+public class MapMath {
+    /**
+     *
+     * @param map
+     * @return максимальное значение (value) коллекции map
+     */
+    public static int getMax(Map map) {
+        int max = Integer.MIN_VALUE;
+        int mapValue;
+        Iterator it = map.values().iterator();
+        while (it.hasNext()) {
+            mapValue = (int) it.next();
+            if (mapValue > max) max = mapValue;
+        }
+        return max;
+    }
+
+    /**
+     *
+     * @param map
+     * @return минимальное значение (value) коллекции map
+     */
+    public static int getMin(Map map) {
+        int min = Integer.MAX_VALUE;
+        int mapValue;
+        Iterator it = map.values().iterator();
+        while (it.hasNext()) {
+            mapValue = (int) it.next();
+            if (mapValue < min) min = mapValue;
+        }
+        return min;
+    }
+
+}
